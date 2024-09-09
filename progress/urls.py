bee446c002aaddf4e8f7ad4b14f170886269c9c9
Shibainu13 <shibainu.auth@gmail.com>
2024-09-09 10:28:59 +0700

    feat: Progress GET and POST methods

--- progress/urls.py
+++ progress/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('goal/', views.goal_view, name='goal_view')
+  path('goal/', views.goal_view, name='goal_view'),
+  path('progress/', views.progress_view, name='progress_view')
 ]
