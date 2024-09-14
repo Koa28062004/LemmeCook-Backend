1d788dbeb457ba341898c0c052d5c38b383b0b0c
Koa28062004 <lnkhoa28062004@gmail.com>
2024-09-14 13:55:37 +0700

    Update: Create progress and goal when register

--- progress/models.py
+++ progress/models.py
@@ -4,11 +4,11 @@ from users.models import Users
 class TodayProgress(models.Model):
     progress_id = models.AutoField(primary_key=True, unique=True)
     user_id = models.ForeignKey(Users, to_field="id", on_delete=models.CASCADE, related_name='progress')
-    date = models.DateField()
-    calories = models.FloatField()
-    fat = models.FloatField()
-    protein = models.FloatField()
-    carb = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+    calories = models.FloatField(default=100)
+    fat = models.FloatField(default=100)
+    protein = models.FloatField(default=100)
+    carb = models.FloatField(default=100)
 
     def __str__(self):
         return f"{self.user_id} - Date: {self.date}"
@@ -16,10 +16,10 @@ class TodayProgress(models.Model):
 class Goal(models.Model):
     goal_id = models.AutoField(primary_key=True, unique=True)
     user_id = models.ForeignKey(Users, to_field="id", on_delete=models.CASCADE)
-    calories = models.FloatField()
-    fat = models.FloatField()
-    protein = models.FloatField()
-    carb = models.FloatField()
+    calories = models.FloatField(default=1000)
+    fat = models.FloatField(default=1000)
+    protein = models.FloatField(default=1000)
+    carb = models.FloatField(default=1000)
 
     def __str__(self):
         return f"{self.user_id} - GoalID: {self.goal_id}"
